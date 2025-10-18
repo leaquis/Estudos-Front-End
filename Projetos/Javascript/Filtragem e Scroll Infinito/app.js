8f0c42c1f1afbce1d7219aefe7421506bfb7ee83
Giovani da Silva Ramos <gi_1997@hotmail.com>
2025-10-18 19:50:43 -0300

    Refatoração

--- Projetos/Javascript/Filtragem e Scroll Infinito/app.js
+++ Projetos/Javascript/Filtragem e Scroll Infinito/app.js
@@ -9,9 +9,8 @@ const getPosts = async() => {
     return await response.json() 
 }
 
-const addPostsIntoDOM = async () => {
-    const posts = await getPosts()
-    const postsTemplate = posts.map(({ id, title, body }) => `
+const generatePostsTemplate = posts => {
+    posts.map(({ id, title, body }) => `
         <div class="post">
             <div class="number">${id}</div>
             <div class="post-info">
@@ -20,15 +19,20 @@ const addPostsIntoDOM = async () => {
             </div>
         </div>
     `).join('')
+}
+
+const addPostsIntoDOM = async () => {
+    const posts = await getPosts()
+    const postsTemplate = generatePostsTemplate(posts)
 
     postsContainer.innerHTML += postsTemplate
 }
 
-addPostsIntoDOM()
-
 const getNextPosts = () => {
-    page++
-    addPostsIntoDOM()
+    setTimeout(() => {
+        page++
+        addPostsIntoDOM()
+    }, 300)
 }
 
 const removeLoader = () => {
@@ -43,28 +47,36 @@ const showLoader = () => {
     removeLoader()
 }
 
-window.addEventListener('scroll', () => {
+const handleScrollToPageBottom = () => {
     const { clientHeight, scrollHeight, scrollTop } = document.documentElement
     const isPageBottomAlmostReached = scrollTop + clientHeight >= scrollHeight - 10
 
     if(isPageBottomAlmostReached) {
         showLoader()
     }
-})
+}
 
-filterInput.addEventListener('input', event => {
-    const inputValue = event.target.value.toLowerCase()
-    const posts = document.querySelectorAll('.post')
-    
-    posts.forEach(post => {
+const showPostIfMatchInputValue = inputValue => post => {
         const postTitle = post.querySelector('.post-title').textContent.toLowerCase()
         const postBody = post.querySelector('.post-body').textContent.toLowerCase()
+        const postContainsInputValue = postTitle.includes(inputValue) || postBody.includes(inputValue)
 
-        if (postTitle.includes(inputValue) || postBody.includes(inputValue)) {
+        if (postContainsInputValue) {
             post.style.display = 'flex'
             return
         }
 
         post.style.display = 'none'
-    })
-})
+    }
+
+const handleInputValue = event => {
+    const inputValue = event.target.value.toLowerCase()
+    const posts = document.querySelectorAll('.post')
+    
+    posts.forEach(showPostIfMatchInputValue(inputValue))
+}
+
+addPostsIntoDOM()
+
+window.addEventListener('scroll', handleScrollToPageBottom)
+filterInput.addEventListener('input', handleInputValue)
