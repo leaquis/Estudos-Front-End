0e5083af3a455ecb6d53a3845bb605cb178af613
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-02-18 02:00:46 -0300

    React + CSS

---
+++ Estudos React/Introducao React/React-CSS/React-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Paragrafo do App.js</p>
+    </div>
+  );
+}
+
+export default App;
