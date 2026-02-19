abac87ea58c366aa8d502481bf1c4e5c8f87821e
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-02-18 21:58:59 -0300

    React-CSS inline

--- Estudos React/Introducao React/React-CSS/React-CSS/src/App.jsx
+++ Estudos React/Introducao React/React-CSS/React-CSS/src/App.jsx
@@ -7,6 +7,16 @@ function App() {
       <h1>React com CSS</h1>
       <MyComponent />
       <p>Paragrafo do App.js</p>
+      <p
+        style={{
+          color: "blue",
+          background: "green",
+          padding: "25px",
+          borderTop: "2px solid red",
+        }}
+      >
+        Este elemento foi estilizado de forma inline
+      </p>
     </div>
   );
 }
