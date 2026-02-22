dcc4bae01fcddbbfc6cbecafb407f8940268712f
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-02-21 22:01:44 -0300

    Aplicação dinamica do CSS-inline e de classes

--- Estudos React/Introducao React/React-CSS/React-CSS/src/App.jsx
+++ Estudos React/Introducao React/React-CSS/React-CSS/src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Giovani");
+  const redTitle = true;
+
   return (
     <div className="App">
       <h1>React com CSS</h1>
@@ -17,6 +22,21 @@ function App() {
       >
         Este elemento foi estilizado de forma inline
       </p>
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS Dinamico
+      </h2>
+      <h2
+        style={
+          name === "Giovani"
+            ? { color: "green", backgroundColor: "yellow" }
+            : null
+        }
+      >
+        CSS Dinamico
+      </h2>
+      <h2 className={redTitle === true ? "red-title" : "title"}>
+        Este titulo vai ter uma classe dinamica
+      </h2>
     </div>
   );
 }
