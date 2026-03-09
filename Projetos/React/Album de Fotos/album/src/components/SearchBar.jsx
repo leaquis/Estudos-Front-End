ee916e90304fda26f6620a2db854208e3bdd5046
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-03-09 02:06:00 -0300

    Fim do projeto de album de fotos

--- Projetos/React/Album de Fotos/album/src/components/SearchBar.jsx
+++ Projetos/React/Album de Fotos/album/src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ setQuery, setCategoria, setActivateSearch }) => {
   const categorias = [
     "Natureza",
     "Pessoas",
@@ -10,9 +10,18 @@ const SearchBar = () => {
   ];
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Pesquisar Fotos..." />
-      <button>Pesquisar</button>
-      <select>
+      <input
+        type="text"
+        placeholder="Pesquisar Fotos..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={() => setActivateSearch(true)}>Pesquisar</button>
+      <select
+        onChange={(e) => {
+          setCategoria(e.target.value);
+          setActivateSearch(true);
+        }}
+      >
         {categorias.map((categoria) => (
           <option key={categoria} value={categoria}>
             {categoria}
