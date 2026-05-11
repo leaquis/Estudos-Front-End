48e2f74bb7d0c78e442adcd7bd7976cc464e76ad
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-05-11 02:33:53 -0300

    Consumo dos states e evento de inserção de letras

--- Projetos/React/Acerte as palavras/AcerteAsPalavras/src/App.jsx
+++ Projetos/React/Acerte as palavras/AcerteAsPalavras/src/App.jsx
@@ -22,6 +22,11 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState([]);
+
   const pickWordAndPickCategory = () => {
     const categories = Object.keys(words);
     const category =
@@ -42,13 +47,13 @@ function App() {
 
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
   };
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   };
 
   const retry = () => {
@@ -58,7 +63,18 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
       {gameStage === "end" && <GameOver restartGame={retry} />}
     </div>
   );
