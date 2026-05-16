81d1653c63309722620943a2f8075927b5f3a313
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-05-16 04:34:56 -0300

    Processando se a letra é certa ou errada

--- Projetos/React/Acerte as palavras/AcerteAsPalavras/src/App.jsx
+++ Projetos/React/Acerte as palavras/AcerteAsPalavras/src/App.jsx
@@ -53,8 +53,30 @@ function App() {
   };
 
   const verifyLetter = (letter) => {
-    console.log(letter);
+    const normalizedLetter = letter.toLowerCase();
+
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+    }
   };
+
+  console.log(guessedLetters);
+  console.log(wrongLetters);
 
   const retry = () => {
     setGameStage(stages[0].name);
