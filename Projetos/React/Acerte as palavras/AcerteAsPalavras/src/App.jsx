a5622daacfe36da5850b3babdea9e42d7c4a19eb
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-05-24 18:03:58 -0300

    Logica do fim de jogo

--- Projetos/React/Acerte as palavras/AcerteAsPalavras/src/App.jsx
+++ Projetos/React/Acerte as palavras/AcerteAsPalavras/src/App.jsx
@@ -14,6 +14,8 @@ const stages = [
   { id: 3, name: "end" },
 ];
 
+const guessesQty = 3;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
@@ -24,7 +26,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState([]);
 
   const pickWordAndPickCategory = () => {
@@ -72,13 +74,30 @@ function App() {
         ...actualWrongLetters,
         normalizedLetter,
       ]);
+
+      setGuesses((actualGuessed) => actualGuessed - 1);
     }
   };
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
+  useEffect(() => {
+    if (guesses <= 0) {
+      clearLetterStates();
+
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
 
   console.log(guessedLetters);
   console.log(wrongLetters);
 
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
     setGameStage(stages[0].name);
   };
 
@@ -97,7 +116,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === "end" && <GameOver restartGame={retry} />}
+      {gameStage === "end" && <GameOver restartGame={retry} score={score} />}
     </div>
   );
 }
