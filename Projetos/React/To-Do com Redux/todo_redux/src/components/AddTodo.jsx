33b3888e6d3312bd2f9c22df11b39ccdea477a1c
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-07-26 03:40:43 -0300

    implementa formulário para adicionar tarefas e estiliza lista de tarefas

--- Projetos/React/To-Do com Redux/todo_redux/src/components/AddTodo.jsx
+++ Projetos/React/To-Do com Redux/todo_redux/src/components/AddTodo.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AddTodo = () => {
-  return <div>AddTodo</div>;
+  return (
+    <form>
+      <input type="text" placeholder="Adicione uma tarefa..." />
+      <button type="submit">Enviar</button>
+    </form>
+  );
 };
 
 export default AddTodo;
--- Projetos/React/To-Do com Redux/todo_redux/src/components/TodoList.jsx
+++ Projetos/React/To-Do com Redux/todo_redux/src/components/TodoList.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <div>
+      <button>Todas</button>
+      <button>Completas</button>
+      <button>Incompletas</button>
+      <ul>
+        <li>
+          <span className="line-trough">Algum texto de tarefa</span>
+          <button>Remover</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default TodoList;
