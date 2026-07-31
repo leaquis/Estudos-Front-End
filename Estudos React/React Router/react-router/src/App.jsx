56c255dfc64f0779a40b3cdcd435c53000366ae3
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-07-31 02:18:45 -0300

    Navegação entre Paginas

--- Estudos React/React Router/react-router/src/App.jsx
+++ Estudos React/React Router/react-router/src/App.jsx
@@ -5,11 +5,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
